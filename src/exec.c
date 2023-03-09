f1f9e432e32d9702c5a2c8aa34c9cabe746f4af7
Oguzhan Akyuz <oakyuz@k2m09s04.42kocaeli.com.tr>
2023-03-09 07:07:56 +0300

    lexer and exec base

---
+++ src/exec.c
@@ -0,0 +1,48 @@
+#include "../lib/minishell.h"
+
+void	exec(t_lextlist *lst, char **env, t_lextlist *prev, t_lextlist *tmp)
+{
+
+	if (tmp->type == PIPE)
+	{
+		pipe(tmp->fd);
+	}
+	if (!fork())
+	{
+		if (prev && prev->type == PIPE)
+		{
+			close(prev->fd[1]);
+			dup2(prev->fd[0], 0);
+			close(prev->fd[0]);
+		}
+		if (tmp->type == PIPE)
+		{
+			printf("dondupipe\n");
+			close(tmp->fd[0]);
+			dup2(tmp->fd[1], 1);
+			close(tmp->fd[1]);
+		}
+		/*
+		else if (tmp->type == EXE && prev && prev->type == PIPE)
+		{
+			dup2(0, prev->fd[0]);
+			close(prev->fd[0]);
+		}
+		*/
+		execve(tmp->command[0], &tmp->command[1], env);
+		exit(1);
+	}
+
+	if (prev && prev->type == PIPE)
+	{
+		close(prev->fd[0]);
+	}
+	if (tmp->type == PIPE)
+	{
+		close(tmp->fd[1]);
+	}
+	prev = tmp;
+
+	if (tmp && tmp->next)
+		exec(lst, env, prev, tmp->next);
+}
